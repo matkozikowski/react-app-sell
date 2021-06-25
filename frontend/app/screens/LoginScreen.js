d10f91bbb6bd967643caed396825d935624ef542
Mateusz Kozikowski <matkozikowski@gmail.com>
2021-06-25 17:13:14 +0200

    Update changes

--- frontend/app/screens/LoginScreen.js
+++ frontend/app/screens/LoginScreen.js
@@ -1,77 +1,81 @@
-import React from 'react';
-import { StyleSheet, Image } from 'react-native';
-import * as Yup from 'yup';
+import React, { useState } from "react";
+import { StyleSheet, Image } from "react-native";
+import * as Yup from "yup";
 
-import Screen from '../components/Screen';
-import { ErrorMessage, Form, FormField, SubmitButton } from '../components/forms';
-import authApi from '../api/auth';
-import { useState } from 'react/cjs/react.development';
-import useAuth from '../auth/useAuth';
+import Screen from "../components/Screen";
+import {
+  ErrorMessage,
+  Form,
+  FormField,
+  SubmitButton,
+} from "../components/forms";
+import authApi from "../api/auth";
+import useAuth from "../auth/useAuth";
 
 const validationSchema = Yup.object().shape({
-    email: Yup.string().required().email().label('Email'),
-    password: Yup.string().required().min(4).label('Password')
+  email: Yup.string().required().email().label("Email"),
+  password: Yup.string().required().min(4).label("Password"),
 });
 
 function LoginScreen(props) {
-    const { logIn } = useAuth();
-    const [loginFailed, setLoginFailed] = useState(false);
+  const { logIn } = useAuth();
+  const [loginFailed, setLoginFailed] = useState(false);
 
-    const handleSubmit = async ({ email, password}) => {
-        const result = await authApi.login(email, password)
+  const handleSubmit = async ({ email, password }) => {
+    const result = await authApi.login(email, password);
 
-        if (!result.ok) return setLoginFailed(true);
-        setLoginFailed(false);
-        logIn(result.data);
-    }
+    if (!result.ok) return setLoginFailed(true);
+    setLoginFailed(false);
+    logIn(result.data);
+  };
 
-    return (
-        <Screen style={styles.container}>
-            <Image 
-                style={styles.logo}
-                source={require('../assets/logo-red.png')} 
-            />
-            <ErrorMessage error="Invalid email and/or password." visible={loginFailed} />
-            <Form
-                initialValues={{ email: '', password: '' }}
-                onSubmit={handleSubmit}
-                validationSchema={validationSchema}
-            >
-                <FormField 
-                    autoCapitalize='none'
-                    autoCorrect={false}
-                    icon='email'
-                    name='email'
-                    keyboardType='email-address'
-                    placeholder='email'
-                    textContentType='emailAddress'
-                />
-                <FormField 
-                    autoCapitalize='none'
-                    autoCorrect={false}
-                    icon='lock'
-                    name='password'
-                    placeholder='Password'
-                    secureTextEntry={true}
-                    textContentType='password'
-                />
-                <SubmitButton title='Login' />
-            </Form>
-        </Screen>
-    );
+  return (
+    <Screen style={styles.container}>
+      <Image style={styles.logo} source={require("../assets/logo-red.png")} />
+      <ErrorMessage
+        error="Invalid email and/or password."
+        visible={loginFailed}
+      />
+      <Form
+        initialValues={{ email: "", password: "" }}
+        onSubmit={handleSubmit}
+        validationSchema={validationSchema}
+      >
+        <FormField
+          autoCapitalize="none"
+          autoCorrect={false}
+          icon="email"
+          name="email"
+          keyboardType="email-address"
+          placeholder="email"
+          textContentType="emailAddress"
+        />
+        <FormField
+          autoCapitalize="none"
+          autoCorrect={false}
+          icon="lock"
+          name="password"
+          placeholder="Password"
+          secureTextEntry={true}
+          textContentType="password"
+        />
+        <SubmitButton title="Login" />
+      </Form>
+    </Screen>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        padding: 10
-    },
-    logo: {
-        width: 80,
-        height: 80,
-        alignSelf: 'center',
-        marginTop: 50,
-        marginBottom: 20
-    }
-})
+  container: {
+    padding: 10,
+  },
+  logo: {
+    width: 80,
+    height: 80,
+    alignSelf: "center",
+    marginTop: 50,
+    marginBottom: 20,
+  },
+});
 
 export default LoginScreen;
